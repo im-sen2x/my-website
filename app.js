7b84be266bfc6a25805ee0ebea891aab8121c6d2
unknown <lopez.jansend@gmail.com>
2020-01-26 00:41:51 +0800

    added static folder

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-let Express = require("express")
-app = Express()
+let express = require("express")
+app = express()
+
+
+app.use(express.static("static"))
+
+app.get("/", (req, res) => {
+    res.send("./static/index.html")
+    // res.send("12314")
+})
 
 PORT = process.env.PORT || 3000 
-app.listen(() => console.log(`Node JS running on PORT: ${PORT}`))
+app.listen(PORT, () => console.log(`Node JS running on PORT: ${PORT}`))
